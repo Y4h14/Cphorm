dee3bbed0922c53f72f9c72927dc931bad2940c7
oovaa <omarvx211@gmail.com>
2025-06-22 12:02:24 +0200

    fix: update Supabase client to use ANON_KEY instead of SERVICE_ROLE_KEY

--- form/server/models/supabase_client.js
+++ form/server/models/supabase_client.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
